3e1501c8aa68c9800a98e42bd06670da808e9bdc
AnikiFan <2254298@tongji.edu.cn>
2024-12-22 13:12:24 +0800

    update Vanilla Gaussian Process

--- MyGP.py
+++ MyGP.py
@@ -2,13 +2,14 @@ import numpy as np
 from scipy.optimize import minimize
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import os
 from kernel import *
 from utils import *
 import seaborn as sns
 
 
 class GP:
-    def __init__(self, f, kernel, theta, bound, noise=1e-5):
+    def __init__(self, kernel, theta, bound, f=None,noise=1e-5,nugget=1e-10):
         """
 
         :param f: y = f(X)
@@ -22,6 +23,7 @@ class GP:
         self.theta = theta
         self.noise = noise
         self.bound = bound
+        self.nugget = nugget
         self._X = None
         self._y = None
         self._m = None
@@ -29,46 +31,59 @@ class GP:
         self._X_test = None
         self._y_test = None
 
-    def fit(self, X):
+    def fit(self, X,y=None):
+        assert y is not None or self.f is not None
         self._X = X
-        self._y = self.f(X)
+        self._y = y or self.f(X)
 
-        def marginal_likelihood(theta):
-            K = self.kernel(self._X, self._X, *theta) + self.noise * np.eye(self._X.shape[0])
+        def marginal_likelihood(hyperparameters):
+            theta = hyperparameters[:-1]
+            noise = hyperparameters[-1]
+            K = self.kernel(self._X, self._X, *theta) + noise * np.eye(self._X.shape[0])
             return np.sum(np.log(np.diagonal(np.linalg.cholesky(K)))) + \
                 0.5 * self._y.T @ np.linalg.inv(K) @ self._y + \
                 0.5 * self._X.shape[0] * np.log(2 * np.pi)
 
-        res = minimize(marginal_likelihood, x0=self.theta, bounds=self.bound, method='L-BFGS-B')
-        self.theta = res.x
+        x0 = self.theta
+        x0.append(self.noise)
+        bounds = self.bound
+        bounds.append([1e-5,None])
+        res = minimize(marginal_likelihood, x0=x0, bounds=bounds, method='L-BFGS-B')
+        self.theta = res.x[:-1]
+        self.noise = res.x[-1]
 
-    def predict(self, X_test):
+    def predict(self, X_test,y_test=None):
         self._X_test = X_test
+        if y_test is not None:
+            self._y_test = y_test
+        elif self.f is not None:
+            self._y_test = self.f(X_test)
         C_N = self.kernel(self._X, self._X, *self.theta) + self.noise * np.eye(self._X.shape[0])
         k = self.kernel(self._X, self._X_test, *self.theta)
-        c = self.kernel(self._X_test, self._X_test, *self.theta) + self.noise * np.eye(self._X_test.shape[0])
+        c = self.kernel(self._X_test, self._X_test, *self.theta) + self.nugget * np.eye(self._X_test.shape[0])
         C_N_inv = np.linalg.inv(C_N)
         m = k.T @ C_N_inv @ self._y
-        cov = c - k.T @ C_N_inv @ k
-        s = np.sqrt(np.where(np.diag(cov) > 0, np.diag(cov), 0))
+        cov = np.clip(c - k.T @ C_N_inv @ k,1e-15,np.inf)
+        s = np.sqrt(np.diag(cov) + self.noise)
         self._m = m
         self._s = s
         return m, s
 
     def plot(self):
         plt.plot(self._X_test.flatten(), self._m.flatten(), label=r'$\mu$')
-        plt.plot(self._X_test.flatten(), self.f(self._X_test).flatten(), label=r'$f(x)$')
+        if self._y_test is not None:
+            plt.plot(self._X_test.flatten(), self._y_test.flatten(), label=r'$f(x)$')
         plt.scatter(self._X.flatten(), self._y.flatten(), label='train data', marker='x', color='black')
         plt.fill_between(self._X_test.flatten(), self._m.flatten() - 1.96 * self._s.flatten(),
                          self._m.flatten() + 1.96 * self._s.flatten(), alpha=0.2, label=r'$\mu\pm 2\sigma$')
         plt.legend()
-        plt.show()
 
 
 class SparseGP:
-    def __init__(self, f, kernel, theta, bound, noise=1e-5, nugget=1e-10):
+    def __init__(self, kernel, theta, bound,f=None, noise=1e-5, nugget=1e-10):
         """
 
+        :param y: 训练集标签
         :param f: y = f(X)
         :param kernel: kernel(X,Y,*theta)，其中theta为超参数列表
         :param theta: 超参数初始值
@@ -83,19 +98,26 @@ class SparseGP:
         self.bound = bound
         self.nugget = nugget
         self._X = None
-        self._y = None
         self._Z = None
         self._m = None
         self._s = None
         self._X_test = None
         self._y_test = None
 
-    def fit(self, X, nz):
+    def fit(self, X, nz,y=None):
+        """
+
+        :param X: 测试集
+        :param nz: pseudo data的个数
+        :param y: 测试集标签，如果之前已经提供了f则可以不提供
+        :return:
+        """
+        assert y is not None or self.f is not None
         self._X = X
         kmeans = KMeans(n_clusters=nz)
         kmeans.fit(X)
         self._Z = kmeans.cluster_centers_
-        self._y = self.f(X)
+        self._y = y or self.f(X)
         def marginal_likelihood(hyperparameters):
             theta = hyperparameters[:-1]
             noise = hyperparameters[-1]
@@ -121,8 +143,12 @@ class SparseGP:
         self.theta = res.x[:-1]
         self.noise = res.x[-1]
 
-    def predict(self, X_test):
+    def predict(self, X_test,y_test=None):
         self._X_test = X_test
+        if y_test is not None:
+            self._y_test = y_test
+        elif self.f is not None:
+            self._y_test = self.f(X_test)
         Knn = np.concatenate([self.kernel(self._X[[i], :], self._X[[i], :], *self.theta) for i in range(self._X.shape[0])]).T
         Kmm = self.kernel(self._Z, self._Z, *self.theta) + self.nugget * np.eye(self._Z.shape[0])
         Kmn = self.kernel(self._Z, self._X, *self.theta)
@@ -146,32 +172,39 @@ class SparseGP:
 
     def plot(self):
         plt.plot(self._X_test.flatten(), self._m.flatten(), label=r'$\mu$')
-        plt.plot(self._X_test.flatten(), self.f(self._X_test).flatten(), label=r'$f(x)$')
+        if self._y_test is not None:
+            plt.plot(self._X_test.flatten(), self._y_test.flatten(), label=r'$f(x)$')
         plt.scatter(self._X.flatten(), self._y.flatten(), label='train data', marker='x', color='black')
         plt.fill_between(self._X_test.flatten(), self._m.flatten() - 1.96 * self._s.flatten(),
                          self._m.flatten() + 1.96 * self._s.flatten(), alpha=0.2, label=r'$\mu\pm 2\sigma$')
-        sns.rugplot(self._Z.flatten(), label='pseudo data')
+        sns.rugplot(self._Z.flatten(), label='pseudo data',lw=1)
         plt.legend()
 
 
 if __name__ == '__main__':
-    # X = np.random.uniform(low=-1, high=1, size=(10, 1))
-    # X_test = np.linspace(-1, 1, 100).reshape(-1, 1)
-    # gp = GP(test_1D, rbf, (1, 1), ((1e-5, None), (1e-5, None)), noise=1e-5)
-    # gp.fit(X)
-    # gp.predict(X_test)
-    # gp.plot()
-
-    plt.figure(figsize=(16, 8))
     X = np.random.uniform(low=-1, high=1, size=(20, 1))
     X_test = np.linspace(-1, 1, 100).reshape(-1, 1)
+
+    plt.figure(figsize=(16, 8))
+    gp = GP(f=test_1D, kernel=rbf, theta=[1, 1], bound=[[1e-5, None], [1e-5, None]])
+    gp.fit(X)
+    gp.predict(X_test)
+    gp.plot()
+    plt.tight_layout()
+    plt.show(dpi=600)
+    plt.savefig(os.path.join(os.curdir,'fig','GP.png'),dpi=600,transparent=True)
+
+    plt.figure(figsize=(16, 8))
     for i in range(6):
         plt.subplot(3, 2, i + 1)
         nz = 3*(i+1)
-        sgp = SparseGP(test_1D, rbf, [1,1], [[1e-5,None],[1e-5,None]])
+        sgp = SparseGP(f = test_1D, kernel = rbf, theta = [1,1], bound = [[1e-5,None],[1e-5,None]])
         sgp.fit(X, nz)
         sgp.predict(X_test)
         sgp.plot()
         plt.title(f'#pseudo data = {nz}')
     plt.tight_layout()
     plt.show(dpi=600)
+    plt.savefig(os.path.join(os.curdir,'fig','SGP.png'),dpi=600,transparent=True)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+# Gaussian Process
 This is a repo for Gaussian process implementation
+
+## Vanilla Gaussian Process
+
+![Vanilla Gaussian Process](./fig/GP.png)
+
+## Sparse Gaussian Process
+
+![Sparse Gaussian Process](./fig/SGP.png)
+
+see `Introduction.pdf` for analysis
