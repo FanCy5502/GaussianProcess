c53e34a6ded978678326f51eb28c6847669c9e72
AnikiFan <2254298@tongji.edu.cn>
2024-12-22 13:15:08 +0800

    update fig

--- MyGP.py
+++ MyGP.py
@@ -191,7 +191,6 @@ if __name__ == '__main__':
     gp.predict(X_test)
     gp.plot()
     plt.tight_layout()
-    plt.show(dpi=600)
     plt.savefig(os.path.join(os.curdir,'fig','GP.png'),dpi=600,transparent=True)
 
     plt.figure(figsize=(16, 8))
@@ -204,7 +203,6 @@ if __name__ == '__main__':
         sgp.plot()
         plt.title(f'#pseudo data = {nz}')
     plt.tight_layout()
-    plt.show(dpi=600)
     plt.savefig(os.path.join(os.curdir,'fig','SGP.png'),dpi=600,transparent=True)
 
 
